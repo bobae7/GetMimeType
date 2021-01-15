8d1a6fdb41638591497823d01ab968928add5573
WooBae, Kim <bobae.red@gmail.com>
2021-01-15 10:49:25 +0900

    초기 데이터 업로드

---
+++ GetMimeType/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMimeType
+{
+    class Program
+    {
+        private static readonly byte[] BMP = { 66, 77 };
+        private static readonly byte[] DOC = { 208, 207, 17, 224, 161, 177, 26, 225 };
+        private static readonly byte[] EXE_DLL = { 77, 90 };
+        private static readonly byte[] GIF = { 71, 73, 70, 56 };
+        private static readonly byte[] ICO = { 0, 0, 1, 0 };
+        private static readonly byte[] JPG = { 255, 216, 255 };
+        private static readonly byte[] MP3 = { 255, 251, 48 };
+        private static readonly byte[] OGG = { 79, 103, 103, 83, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0 };
+        private static readonly byte[] PDF = { 37, 80, 68, 70, 45, 49, 46 };
+        private static readonly byte[] PNG = { 137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82 };
+        private static readonly byte[] PSD = { 38, 42, 50, 53 };
+        private static readonly byte[] RAR = { 82, 97, 114, 33, 26, 7, 0 };
+        private static readonly byte[] SWF = { 70, 87, 83 };
+        private static readonly byte[] TIFF = { 73, 73, 42, 0 };
+        private static readonly byte[] TORRENT = { 100, 56, 58, 97, 110, 110, 111, 117, 110, 99, 101 };
+        private static readonly byte[] TTF = { 0, 1, 0, 0, 0 };
+        private static readonly byte[] WAV_AVI = { 82, 73, 70, 70 };
+        private static readonly byte[] WMV_WMA = { 48, 38, 178, 117, 142, 102, 207, 17, 166, 217, 0, 170, 0, 98, 206, 108 };
+        private static readonly byte[] ZIP_DOCX = { 80, 75, 3, 4 };
+
+        [DllImport(@"urlmon.dll", CharSet = CharSet.Auto)]
+        private extern static System.UInt32 FindMimeFromData(
+            System.UInt32 pBC,
+            [MarshalAs(UnmanagedType.LPStr)] System.String pwzUrl,
+            [MarshalAs(UnmanagedType.LPArray)] byte[] pBuffer,
+            System.UInt32 cbSize,
+            [MarshalAs(UnmanagedType.LPStr)] System.String pwzMimeProposed,
+            System.UInt32 dwMimeFlags,
+            out System.UInt32 ppwzMimeOut,
+            System.UInt32 dwReserverd
+        );
+
+        static void Main(string[] args)
+        {
+        }
+
+        static string GetFileType(string file_name)
+        {
+            string mime = "application/octet-stream"; //DEFAULT UNKNOWN MIME TYPE
+
+            //Ensure that the filename isn't empty or null
+            if (string.IsNullOrWhiteSpace(file_name))
+            {
+                return mime;
+            }
+
+            //Get the file extension
+            string extension = Path.GetExtension(file_name) == null
+                                   ? string.Empty
+                                   : Path.GetExtension(file_name).ToUpper();
+
+            using (FileStream stream = new FileStream(file_name, FileMode.Open, FileAccess.Read))
+            {
+                int stream_length = 200;
+                if (stream.Length < 200)
+                {
+                    stream_length = (int)stream.Length;
+                }
+                byte[] file = new byte[stream_length];
+
+                int n = stream.Read(file, 0, stream_length);
+
+                //Get the MIME Type
+                if (file.Take(2).SequenceEqual(BMP))
+                {
+                    mime = "image/bmp";
+                }
+                else if (file.Take(8).SequenceEqual(DOC))
+                {
+                    mime = "application/msword";
+                }
+                else if (file.Take(2).SequenceEqual(EXE_DLL))
+                {
+                    mime = "application/x-msdownload"; //both use same mime type
+                }
+                else if (file.Take(4).SequenceEqual(GIF))
+                {
+                    mime = "image/gif";
+                }
+                else if (file.Take(4).SequenceEqual(ICO))
+                {
+                    mime = "image/x-icon";
+                }
+                else if (file.Take(3).SequenceEqual(JPG))
+                {
+                    mime = "image/jpeg";
+                }
+                else if (file.Take(3).SequenceEqual(MP3))
+                {
+                    mime = "audio/mpeg";
+                }
+                else if (file.Take(14).SequenceEqual(OGG))
+                {
+                    if (extension == ".OGX")
+                    {
+                        mime = "application/ogg";
+                    }
+                    else if (extension == ".OGA")
+                    {
+                        mime = "audio/ogg";
+                    }
+                    else
+                    {
+                        mime = "video/ogg";
+                    }
+                }
+                else if (file.Take(7).SequenceEqual(PDF))
+                {
+                    mime = "application/pdf";
+                }
+                else if (file.Take(16).SequenceEqual(PNG))
+                {
+                    mime = "image/png";
+                }
+                else if (file.Take(4).SequenceEqual(PSD))
+                {
+                    mime = "application/x-photoshop";
+                }
+                else if (file.Take(7).SequenceEqual(RAR))
+                {
+                    mime = "application/x-rar-compressed";
+                }
+                else if (file.Take(3).SequenceEqual(SWF))
+                {
+                    mime = "application/x-shockwave-flash";
+                }
+                else if (file.Take(4).SequenceEqual(TIFF))
+                {
+                    mime = "image/tiff";
+                }
+                else if (file.Take(11).SequenceEqual(TORRENT))
+                {
+                    mime = "application/x-bittorrent";
+                }
+                else if (file.Take(5).SequenceEqual(TTF))
+                {
+                    mime = "application/x-font-ttf";
+                }
+                else if (file.Take(4).SequenceEqual(WAV_AVI))
+                {
+                    mime = extension == ".AVI" ? "video/x-msvideo" : "audio/x-wav";
+                }
+                else if (file.Take(16).SequenceEqual(WMV_WMA))
+                {
+                    mime = extension == ".WMA" ? "audio/x-ms-wma" : "video/x-ms-wmv";
+                }
+                else if (file.Take(4).SequenceEqual(ZIP_DOCX))
+                {
+                    mime = extension == ".DOCX" ? "application/vnd.openxmlformats-officedocument.wordprocessingml.document" : "application/x-zip-compressed";
+                }
+
+                //byte[] tar = { (byte)137, (byte)80, (byte)78, (byte)71, (byte)13, (byte)10 };
+                //byte[] tar = { 137, 80, 78, 71, 13, 10 };
+
+                //var arr = bytes.Take<byte>(6);
+
+                //var result = StructuralComparisons.StructuralEqualityComparer.Equals(arr.ToArray<byte>(), tar);
+            }
+
+            return mime;
+        }
+
+        static string GetMimeType(string file_name)
+        {
+            byte[] buffer = new byte[256];
+
+            using (FileStream fs = new FileStream(file_name, FileMode.Open, FileAccess.Read))
+            {
+                if (fs.Length >= 256)
+                    fs.Read(buffer, 0, 256);
+                else
+                    fs.Read(buffer, 0, (int)fs.Length);
+            }
+
+            try
+            {
+                System.UInt32 mimetype;
+
+                FindMimeFromData(0, null, buffer, 256, null, 0, out mimetype, 0);
+
+                System.IntPtr mimeTypePtr = new IntPtr(mimetype);
+
+                string mime = Marshal.PtrToStringUni(mimeTypePtr);
+
+                Marshal.FreeCoTaskMem(mimeTypePtr);
+
+                return mime;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return "";
+            }
+        }
+    }
+}
